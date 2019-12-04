a0badb0001265dd6b1a019fc6320a0138d9d032f
bihwal <strength147@gmail.com>
2019-12-04 19:56:00 +0545

    FINAL

--- app/src/main/java/com/example/a2ndassignment/MainActivity.java
+++ app/src/main/java/com/example/a2ndassignment/MainActivity.java
@@ -1,14 +1,106 @@
 package com.example.a2ndassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.a2ndassignment.fragments.AreaofCircleFragment;
+import com.example.a2ndassignment.fragments.AutomorphicFragment;
+import com.example.a2ndassignment.fragments.PalindromeFragment;
+import com.example.a2ndassignment.fragments.ReverseFragment;
+import com.example.a2ndassignment.fragments.ReverseStringFragment;
+import com.example.a2ndassignment.fragments.SumFragment;
 
+public class MainActivity extends AppCompatActivity{
+
+    Button btnSum,btnArea,btnReverseNumber,btnPalindrome,btnautoMorphicNumber,btnReverseString;
+
+   // private String status;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSum=findViewById(R.id.btnAdd);
+        btnArea=findViewById(R.id.btnArea);
+        btnReverseNumber=findViewById(R.id.btnReverseNumber);
+        btnPalindrome=findViewById(R.id.btnPalindrome);
+        btnautoMorphicNumber=findViewById(R.id.btnAutomorphic);
+        btnReverseString=findViewById(R.id.btnReverseString);
+
+        btnSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                SumFragment sumFragment=new SumFragment();
+                fragmentTransaction.replace(R.id.fragmentcontainer,sumFragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+
+        btnArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                AreaofCircleFragment areaofCircle=new AreaofCircleFragment();
+                fragmentTransaction.replace(R.id.fragmentcontainer,areaofCircle);
+                fragmentTransaction.commit();
+            }
+        });
+
+        btnReverseNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                ReverseFragment reverseFragment=new ReverseFragment();
+                fragmentTransaction.replace(R.id.fragmentcontainer,reverseFragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+        btnPalindrome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                PalindromeFragment palindromeFragment=new PalindromeFragment();
+                fragmentTransaction.replace(R.id.fragmentcontainer,palindromeFragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+        btnautoMorphicNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                AutomorphicFragment automorphicFragment=new AutomorphicFragment();
+                fragmentTransaction.replace(R.id.fragmentcontainer,automorphicFragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+
+        btnReverseString.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                ReverseStringFragment stringRevFragment=new ReverseStringFragment();
+                fragmentTransaction.replace(R.id.fragmentcontainer,stringRevFragment);
+                fragmentTransaction.commit();
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/a2ndassignment/fragments/SumFragment.java
+++ app/src/main/java/com/example/a2ndassignment/fragments/SumFragment.java
@@ -1,111 +1,57 @@
 package com.example.a2ndassignment.fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.a2ndassignment.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SumFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SumFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
-public class SumFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class SumFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public SumFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SumFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SumFragment newInstance(String param1, String param2) {
-        SumFragment fragment = new SumFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    EditText etfirst,etsecond;
+    Button btnadd;
+    TextView tvadd;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sum, container, false);
-    }
+        View view= inflater.inflate(R.layout.fragment_sum, container, false);
+        etfirst=view.findViewById(R.id.etfirst);
+        etsecond=view.findViewById(R.id.etsecond);
+        btnadd=view.findViewById(R.id.btnaddnums);
+        tvadd=view.findViewById(R.id.tvadd);
+        btnadd.setOnClickListener(this);
+        return view;
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+    public void onClick(View v) {
+        if (TextUtils.isEmpty(etfirst.getText())) {
+            etfirst.setError("Enter Something");;
+            return;
+        }
+        if (TextUtils.isEmpty(etsecond.getText())) {
+            etsecond.setError("Enter Something");;
+            return;
+        }
+        int first= Integer.parseInt(etfirst.getText().toString());
+        int second= Integer.parseInt(etsecond.getText().toString());
+        int result=first+second;
+        tvadd.setText("Result is : "+result);
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
